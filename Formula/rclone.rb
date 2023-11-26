52dfcb19f55dce4c0bb3b480b6ede0d2696e9e75
Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>
2023-11-26 23:06:03 +0530

    Remove bottles
    Signed-off-by: Anagh Kumar Baranwal <6824881+darthShadow@users.noreply.github.com>

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -6,16 +6,6 @@ class Rclone < Formula
   license "MIT"
   head "https://github.com/rclone/rclone.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "d4afe9d2f7486a3dddba567553bb03225b4fb190cff7e3f746ff3017b0cc4c31"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a8c2403d41ff5403491f22da9293cc57e8a3053105d9822ed418a5db41e80088"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "22b3103ac7971e23e1b6b408e554fccc5ebc878d439b047be07e17142419cea4"
-    sha256 cellar: :any_skip_relocation, sonoma:         "5b2c368073e7ce78737f25febd9006bcc232d07e768b96c24775e87c5422e55d"
-    sha256 cellar: :any_skip_relocation, ventura:        "899aa8fe00ccb9ed1acf2eb8948b9698847159b086c727f6f9a3b0cf3513936c"
-    sha256 cellar: :any_skip_relocation, monterey:       "e5d950991458913aa1ddc78faf25f4f52f5065248a385c62c65dda9d463d52d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a8d2b8e0c5760f8567190e2db6862fe18fc2b71e8f94482e79a26ec1b60df0d"
-  end
-
   depends_on "go" => :build
 
   def install
